9f039a5eb74e649541c5e5a490bc5c908ba99c63
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-08-16 15:51:27 -0700

    updated with 2 more lessons

--- App.js
+++ App.js
@@ -4,12 +4,34 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Ryan', age: 37},
+      {name: 'Michelle', age: 57},
+      {name: 'Olga', age: 23}
+    ],
+    otherState: 'some other rando values'
+  }
+  switchNameHandler = () => {
+  //  DONT DO THIS this.state.persons[0].name
+    this.setState( {
+      persons:[
+        {name: 'Ryan awesome', age: 37},
+        {name: 'Michelle', age: 57},
+        {name: 'Olga', age: 23}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
-      <h1>Test</h1>
+      <h1>Hi I am a React App</h1>
       <p1>This is working!!!</p1>
-      <Person />
+      <button onClick={this.switchNameHandler}>Switch name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are: Making it super nice</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
--- Person/Person.js
+++ Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Person = () => {
-  return <p>I am a person</p>
+const Person = (props) => {
+  return (
+  <div>
+  <p>I am {props.name} and I am {props.age} years old</p>
+  <p>{props.children}</p>
+  </div>
+  )
 }
 
 export default Person;
