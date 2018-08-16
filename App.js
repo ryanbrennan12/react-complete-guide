1da9e73904b9f7346a9cab832bc6e79f874925f4
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-08-16 11:43:23 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Person from './Person/Person'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>Test</h1>
+      <p1>This is working!!!</p1>
+      <Person />
+      </div>
+    );
+  }
+}
+//gsui
+//testing for initial commit
+export default App;
